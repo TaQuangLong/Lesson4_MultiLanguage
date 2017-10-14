c10b93a83ad9701a16ebb05d9715d8b38a7e412f
long <longtq0807@gmail.com>
2017-10-14 09:46:51 +0700

    Initial commit

---
+++ app/src/main/java/project/quanglong/lesson4_multilanguage/MainActivity.java
@@ -0,0 +1,19 @@
+package project.quanglong.lesson4_multilanguage;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
